e118a5390b1319d13e0dde88d5b7ce843edb33be
Siolamo <oibaf250407@gmail.com>
2025-10-22 10:20:13 +0200

    funzioni

---
+++ funzioni.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/*
+Cos'è una funzione?
+Una funzione non è nient altro che un blocco di codcice eseguibile alla chiamata
+esempio di funzione:
+*/
+int miaFunzione()
+{
+    return 0;
+}
+
+/*
+Studiamo la sintassi:
+int miaFunzione()
+
+int è quello che restituirà la funzione.
+vediamo che alla fine della funzione c'è un 'return 0;', 0 è un intero
+se li al posto di int avessi scritto per esempio char, avrei dovuto mettere un return di tipo char, es: retunrn 'c';
+
+miaFunzione, è il nome della funzione, la posso chiamare come voglio, l'avrei potuta chiamare anche pippo baudo
+
+*/
+
+int main()
+{
+    
+}
+
