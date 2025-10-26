2f225ab98af05b46f8d90612a773bcb76069d969
Siolamo <oibaf250407@gmail.com>
2025-10-26 11:59:52 +0100

    Funzioni completate

--- funzioni.c
+++ funzioni.c
@@ -71,6 +71,7 @@ esempio:
 
 int sommaConParametri(int a, int b)
 {
+    printf("il primo valore è: %d e il secondo è: %d\n",a,b );
     return a+b;
 }
 
@@ -80,12 +81,47 @@ in questo caso abbiamo una funzione che:
 2) sommaConParametri: si chiama così
 3) (int a, int b): richiede due parametri INTERI
 
+I parametri, all'interno della funzione (sommaConParametri), vengono visti come due normalissime variabili,
+in quella funzione avrei potuto usare a e b come volevo, in questo caso, ne sto semplicemente restituendo la somma.
 
+Quindi, ovunque io vado a chiamare questa funzione, ne dovrò specificare i parametri es: (vai a linea n)
+ma andando alla linea n, vediamo che chiamando la funzione così, quando andiamo ad eseguire il programma ci mostra solo il printf che
+vediamo in linea 74, questo perchè con quella chiamata noi e come se stessimo scrivendo _ = a+b nello script senza farci effettivamente niente, senza stamparlo.
+quindi, in questo caso, se vogliamo vedere anche l'effetivo risultato di 'return a+b' dovremmo fare un printf che lo scriva (linea n);
+in questo caso il %d del printf in linea n viene rimpiazzato col valore di 'return a+b' che vediamo in linea 75, dove a e b sono i 
+valori che gli passiamo appunto come parametri, in questo caso a=5 e b=3.
+
+Se invece dobbiamo per esempio, invece che scrivere il risultato di a+b, ci serve tenere quel risultato in una variabile nel main
+per poi andarlo a utilizzare in futuro, possiamo farlo!
+dobbiamo prima di tutto guardare cosa restituisce la funzione (in questo caso con SommaConParametri, restituisce un intero (Linea 80)).
+poi dobbiamo creare una variabile dello stesso tipo (in questo caso int);
+e chiamare la funziona nell'assegnazione del valore della variabile (esempio linea n)
+così la variabile risultato creata a linea n verrà sostituita con il valore di 'return a+b' dove a=7, b=12;
+
+*/
+
+
+/*
+!ATTENZIONE
+! ORA ABBIAMO VISTO 3 MODI PER CHIAMARE UNA FUNZIONE
+?1) int r  = miafunzione();
+?2) miafunzione();
+?3) printf("%d", mianfunzione());
+! BISOGNA RICORDARSI CHE OGNI VOLTA CHE SI CHIAMA UNA FUNZIONE VERRà SEMPRE ESEGUITO
+! TUTTO IL CODICE AL SUO INTERNO
+! QUINDI ANCHE SE HO:
+? int r = sommaConParametri(8,23); //un assegnazione di un valore
+!E DENTRO 'sommaConParametri' HO SCRITTO UN PRINTF
+!IL PRINTF VERRà SCRITTO!
+!(e qualsiasi altra cosa io abbia scritto nella funzione, verrà eseguita)
 */
 
 int main()
 {
-    miaFunzione()
+   //int risultato = sommaConParametri(7,12);
+    miaFunzione();
+    //sommaConParametri(5,3);
+    printf("il risultato è: %d\n",sommaConParametri(5,3));
 }
 
 
